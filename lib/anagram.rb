5ef2dcb96d06b319f38f69fca927c77533e48bc6
kyle Dornan <kyledornan26@gmail.com>
2020-06-07 21:14:55 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,13 +6,6 @@ class Anagram
   end 
   def match(array_of_words)
  return_array = []
-    #array_of_words.each do |element|
-      #(@word.split("").sort) == (element.split("").sort)
-        #return_array << element
-      #end
-    #end
-    #return_array
-    
     array_of_words.select do |element|
       (@word.split("").sort) == (element.split("").sort)
     end
