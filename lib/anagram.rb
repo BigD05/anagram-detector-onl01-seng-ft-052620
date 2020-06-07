a9aeb0bf832041aec7768b85087d49b606049664
kyle Dornan <kyledornan26@gmail.com>
2020-06-07 21:15:00 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :words 
+  
+  def initialize(word)
+    @word  = word 
+  end 
+  def match(array_of_words)
+ return_array = []
+    array_of_words.select do |element|
+      (@word.split("").sort) == (element.split("").sort)
+    end
+  end
+  
+  
+end
+  
+  
+  
+  # Your code goes here!
