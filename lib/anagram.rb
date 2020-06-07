050964037cb78869957ec18332b573977cda982a
kyle Dornan <kyledornan26@gmail.com>
2020-06-07 21:11:40 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram 
+  attr_accessor :words 
+  
+  def initialize(word)
+    @word  = word 
+  end 
+  
+  def anagram_detectiond 
+    word.match 
+
+
+end   
+  
+  end 
+  
+  
+  
+  # Your code goes here!
