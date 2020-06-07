2227fbf8ba12a425c54684be322e69a7acf7a08a
kyle Dornan <kyledornan26@gmail.com>
2020-06-07 21:13:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -4,7 +4,7 @@ class Anagram
   def initialize(word)
     @word  = word 
   end 
-  def 
+  def match()
  return_array = []
     #array_of_words.each do |element|
       #(@word.split("").sort) == (element.split("").sort)
